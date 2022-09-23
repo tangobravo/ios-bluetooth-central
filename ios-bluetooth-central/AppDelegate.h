9e3008c751df713286ad8b743db5936701b461b4
Simon Taylor <simon@zappar.com>
2022-09-23 18:04:09 +0100

    Initial Commit

---
+++ ios-bluetooth-central/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ios-bluetooth-central
+//
+//  Created by Simon Taylor on 23/09/2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
